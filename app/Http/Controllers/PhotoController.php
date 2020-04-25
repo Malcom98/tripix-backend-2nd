9fe0a05fc942c6a753575a9fe67834a43f8c1930
Malcom98 <35956934+Malcom98@users.noreply.github.com>
2020-04-25 22:39:36 +0200

    API - /photo/getphoto - Ako se proslijedi &maxwidth=X&maxheight=X tada se uzimaju te veličine slike. Inače ostaje staro.

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -11,11 +11,18 @@ class PhotoController extends Controller
             return response()->json(["Error"=>"Unauthorized."],401);
         }else*/{
             $photo_reference=$request['photo_reference'];
+            $maxwidth=400;
+            if(isset($request['maxwidth']))
+                $maxwidth=$request['maxwidth'];
             
             if($photo_reference==null)
                 return response()->json(["Error"=>"Bad Request"],400);
-            $url="https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=".$photo_reference."&key=AIzaSyCFOkhSfIYP_i1w5q_Lk-3Rg81dAsCSwcE";
-            
+
+            $url="https://maps.googleapis.com/maps/api/place/photo?maxwidth=".$maxwidth;
+            if(isset($request['maxheight']))
+                $url.="&maxheight=".$request['maxheight'];
+            $url.="&photoreference=".$photo_reference."&key=AIzaSyCFOkhSfIYP_i1w5q_Lk-3Rg81dAsCSwcE";
+
             $image=file_get_contents($url);
             header("Content-Type: image/jpeg");
             echo $image;    
