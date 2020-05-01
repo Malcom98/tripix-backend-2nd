6df088c98901bc9533c331e1c5d995198d61eda3
Malcom98 <35956934+Malcom98@users.noreply.github.com>
2020-05-01 21:40:14 +0200

    HELPER FUNCTION - Shortest Path - Created "createObjectForShortestPath()" function. createObjectForShortestPath() returns JSON data that is required for getShortestPath() function.

--- app/HelperFunctions/ShortestPath.php
+++ app/HelperFunctions/ShortestPath.php
@@ -3,83 +3,87 @@ namespace App\HelperFunctions;
 use Illuminate\Http\Request;
 
 class ShortestPath{
-    public static function getShortestPath(Request $request){
+    //This function is used when shortest path is required
+    //  @origin - Starting point of a route. Has longitude and latitude.
+    //  @destination - Ending point of a route. Has longitude and latitude.
+    //  @waypoints - Waypoints to be visited during the route. Array of objects which contain latitude and longitude.
+    public static function getShortestPath($origin,$destination,$waypoints){
             //Putting data from request body into php variables
-            $origin=$request->origin;
-            $destination=$request->destination;
-            $waypoints=$request->waypoints;
+        /*$origin=$request->origin;
+        $destination=$request->destination;
+        $waypoints=$request->waypoints;*/
 
-            //Getting content
-            $link=self::createLink($origin,$destination,$waypoints);
-            $googleDirectionsResponse = json_decode(file_get_contents($link));
-            //Data needed for response
-            $locations=array();
-            $polyline= $googleDirectionsResponse->routes[0]->overview_polyline->points; // Sada mi se ovo nalazi u stringu
-            $total_distance=0;
-            $total_duration=0;
+        //Getting content
+        $link=self::createLink($origin,$destination,$waypoints);
+        $googleDirectionsResponse = json_decode(file_get_contents($link));
+        //Data needed for response
+        $locations=array();
+        $polyline= $googleDirectionsResponse->routes[0]->overview_polyline->points; // Sada mi se ovo nalazi u stringu
+        $total_distance=0;
+        $total_duration=0;
 
-            //Gathering data for place ids
-            $coordinates=array();
-            foreach($googleDirectionsResponse->routes[0]->waypoint_order as $waypoint){
-                array_push($coordinates,$waypoint);
-            }
-
-            //Adding origin to locations
-            $originObject=self::createOriginObject($origin);
-            array_push($locations,$originObject);
-
-            //Adding landmarks to location
-            $counter=1;
-            $destination_distance;
-            $destination_duration;
-            foreach($googleDirectionsResponse->routes[0]->legs as $path){
-                //Skip destination jer je ona stavljena u legs kao posljednji objekt u arrayu
-                if($counter==count($googleDirectionsResponse->routes[0]->legs)){
-                    $destination_duration=explode(' ',$path->duration->text)[0];
-                    $destination_distance=explode(' ',$path->distance->text)[0];
-                    $total_distance+=explode(' ',$path->distance->text)[0];
-                    $total_duration+=explode(' ',$path->duration->text)[0];
-                    break;
-                }
-
-                $place_latitude=$waypoints[$coordinates[$counter-1]]["lat"];
-                $place_longitude=$waypoints[$coordinates[$counter-1]]["long"];
-                $place_duration=explode(' ',$path->duration->text)[0];
-                $place_distance=explode(' ',$path->distance->text)[0];
-
-                $waypoint=[
-                    //"place_id"=>$place_ids[$counter-1],
-                    //"description"=>$place_descriptions[$counter],
-                    "latitude"=>$place_latitude,
-                    "longitude"=>$place_longitude,
-                    "duration"=>$place_duration,
-                    "distance"=>$place_distance
-                ];
-
-                array_push($locations,$waypoint);
-                $counter++;
-
-                $total_distance+=explode(' ',$path->distance->text)[0];
-                $total_duration+=explode(' ',$path->duration->text)[0];
-            }
-
-            //Adding destination to locations array
-            $destinationObject=self::createDestinationObject($destination,$destination_duration,$destination_distance);
-            array_push($locations,$destinationObject);
-
-            //Forming response object
-            $response_object=[
-                "locations"=>$locations,
-                "route"=>$polyline,
-                "distance"=>$total_distance,
-                "duration"=>$total_duration
-            ];
-
-            //Returning JSON object
-            return json_encode($response_object);
+        //Gathering data for place ids
+        $coordinates=array();
+        foreach($googleDirectionsResponse->routes[0]->waypoint_order as $waypoint){
+            array_push($coordinates,$waypoint);
         }
 
-        //This function is used to generate link for google directions api
+        //Adding origin to locations
+        $originObject=self::createOriginObject($origin);
+        array_push($locations,$originObject);
+
+        //Adding landmarks to location
+        $counter=1;
+        $destination_distance;
+        $destination_duration;
+        foreach($googleDirectionsResponse->routes[0]->legs as $path){
+            //Skip destination jer je ona stavljena u legs kao posljednji objekt u arrayu
+            if($counter==count($googleDirectionsResponse->routes[0]->legs)){
+                $destination_duration=explode(' ',$path->duration->text)[0];
+                $destination_distance=explode(' ',$path->distance->text)[0];
+                $total_distance+=explode(' ',$path->distance->text)[0];
+                $total_duration+=explode(' ',$path->duration->text)[0];
+                break;
+            }
+
+            $place_latitude=$waypoints[$coordinates[$counter-1]]["lat"];
+            $place_longitude=$waypoints[$coordinates[$counter-1]]["long"];
+            $place_duration=explode(' ',$path->duration->text)[0];
+            $place_distance=explode(' ',$path->distance->text)[0];
+
+            $waypoint=[
+                //"place_id"=>$place_ids[$counter-1],
+                //"description"=>$place_descriptions[$counter],
+                "latitude"=>$place_latitude,
+                "longitude"=>$place_longitude,
+                "duration"=>$place_duration,
+                "distance"=>$place_distance
+            ];
+
+            array_push($locations,$waypoint);
+            $counter++;
+
+            $total_distance+=explode(' ',$path->distance->text)[0];
+            $total_duration+=explode(' ',$path->duration->text)[0];
+        }
+
+        //Adding destination to locations array
+        $destinationObject=self::createDestinationObject($destination,$destination_duration,$destination_distance);
+        array_push($locations,$destinationObject);
+
+        //Forming response object
+        $response_object=[
+            "locations"=>$locations,
+            "route"=>$polyline,
+            "distance"=>$total_distance,
+            "duration"=>$total_duration
+        ];
+
+        //Returning JSON object
+        return json_encode($response_object);
+    }
+    
+    //This function is used to generate link for google directions api
     private static function createLink($origin,$destination,$waypoints){//Making google api directions request
         $link = "https://maps.googleapis.com/maps/api/directions/json?origin=".$origin["lat"].",".$origin["long"]."&waypoints=optimize:true|";
         foreach($waypoints as $waypoint){
@@ -88,34 +92,61 @@ class ShortestPath{
         $link.="&destination=".$destination["lat"].",".$destination["long"]."&key=AIzaSyCFOkhSfIYP_i1w5q_Lk-3Rg81dAsCSwcE&mode=driving&language=en&region=undefined";
         
         return $link;
-        }
+    }
 
     //This function creates origin object
     private static function createOriginObject($origin){
-            $originObject=[
-                //"place_id"=>$place_id,
-                //"description"=>$description,
-                "latitude"=>$origin["lat"],
-                "longitude"=>$origin["long"],
-                "duration"=>"0",
-                "distance"=>"0"
-            ];
+        $originObject=[
+            //"place_id"=>$place_id,
+            //"description"=>$description,
+            "latitude"=>$origin["lat"],
+            "longitude"=>$origin["long"],
+            "duration"=>"0",
+            "distance"=>"0"
+        ];
 
-            return $originObject;
-        }
+        return $originObject;
+    }
 
     //This function creates destination object
     private static function createDestinationObject($destination,$duration,$distance){
-            $destinationObject=[
-                //"description"=>$description,
-                "latitude"=>$destination["lat"],
-                "longitude"=>$destination["long"],
-                "duration"=>$duration,
-                "distance"=>$distance
+        $destinationObject=[
+            //"description"=>$description,
+            "latitude"=>$destination["lat"],
+            "longitude"=>$destination["long"],
+            "duration"=>$duration,
+            "distance"=>$distance
+        ];
+
+        return $destinationObject;
+    }
+
+    //This function creates an object that is requested in getShortestPath function
+    //which is actually used for finding the shortest path.
+    public static function createObjectForShortestPath($route){
+        //Get number of attractions
+        $numberOfAttractions=count($route);
+        //Form origin object
+        $origin=["lat"=>$route[0]->latitude,"long"=>$route[0]->longitude];
+        //Form waypoints object
+        $waypoints=array();
+        for($i=1;$i<$numberOfAttractions-1;$i++){
+            $waypoint=[
+                "lat"=>$route[$i]->latitude,
+                "long"=>$route[$i]->longitude
             ];
-
-            return $destinationObject;
+            array_push($waypoints,$waypoint);
         }
+        //Form destination object
+        $destination=["lat"=>$route[$numberOfAttractions-1]->latitude,"long"=>$route[$numberOfAttractions-1]->longitude];
+        //Merge objects into route object
+        $route=[
+            "origin"=>$origin,
+            "waypoints"=>$waypoints,
+            "destination"=>$destination
+        ];
+        //Return response object
+        return $route;
+    }
 }
-
 ?>
