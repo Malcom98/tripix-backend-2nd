c79599b47d305c69a445bc816a65a0d1dbafc0f5
Malcom98 <35956934+Malcom98@users.noreply.github.com>
2020-05-04 21:59:55 +0200

    API - Removing JWT on PhotoController.

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -7,25 +7,21 @@ use Illuminate\Http\Request;
 class PhotoController extends Controller
 {
     public function getPhoto(Request $request){
-        if(!JWTValidation($request)){
-            return response()->json(["Error"=>"Unauthorized."],401);
-        }else{
-            $photo_reference=$request['photo_reference'];
-            $maxwidth=400;
-            if(isset($request['maxwidth']))
-                $maxwidth=$request['maxwidth'];
-            
-            if($photo_reference==null)
-                return response()->json(["Error"=>"Bad Request"],400);
+        $photo_reference=$request['photo_reference'];
+        $maxwidth=400;
+        if(isset($request['maxwidth']))
+            $maxwidth=$request['maxwidth'];
+        
+        if($photo_reference==null)
+            return response()->json(["Error"=>"Bad Request"],400);
 
-            $url="https://maps.googleapis.com/maps/api/place/photo?maxwidth=".$maxwidth;
-            if(isset($request['maxheight']))
-                $url.="&maxheight=".$request['maxheight'];
-            $url.="&photoreference=".$photo_reference."&key=".env("GOOGLE_API_KEY","somedefaultvalue");
+        $url="https://maps.googleapis.com/maps/api/place/photo?maxwidth=".$maxwidth;
+        if(isset($request['maxheight']))
+            $url.="&maxheight=".$request['maxheight'];
+        $url.="&photoreference=".$photo_reference."&key=".env("GOOGLE_API_KEY","somedefaultvalue");
 
-            $image=file_get_contents($url);
-            header("Content-Type: image/jpeg");
-            echo $image;    
-        }
+        $image=file_get_contents($url);
+        header("Content-Type: image/jpeg");
+        echo $image;  
     }
 }
