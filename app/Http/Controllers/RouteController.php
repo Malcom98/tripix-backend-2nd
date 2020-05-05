0cdbd2a99e3583d5fc2d86c85c35d82256ee99f0
Malcom98 <35956934+Malcom98@users.noreply.github.com>
2020-05-05 19:31:19 +0200

    CODE REFACTORING - Commenting functions, commenting function parts, renaming functions due to PHP convention.

--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -18,13 +18,14 @@ class RouteController extends Controller
     //------------------------------------- A P I    F U N C T I O N S ------------------------------------------
     //-----------------------------------------------------------------------------------------------------------
 
-    //Planned route
-    //This function is called when user presses "Create" on "Route Overview" screen.
+    //Function planRoute(Request $request) is used when user accepts the route
+    //that was shown to him on "Route overview screen".
+    //This function saves route and route items into database.    
+    //  @request - Request that was received from user.
     public function planRoute(Request $request){
         //JWT Validation
-        if(!JWTValidation($request)){
+        if(!JWTValidation($request))
             return response()->json(["Error"=>"Unauthorized."],401);
-        }
 
         //Validation
         $rules=[
@@ -37,37 +38,40 @@ class RouteController extends Controller
         ];
 
         $validator=Validator::make($request->all(),$rules);
-        if($validator->fails()){
+        if($validator->fails())
             return response()->json($validator->errors(),400);
-        }
 
         //Save data into table routes
-        self::SavePlannedRoute($request);
+        self::savePlannedRoute($request);
         $userRoutes=Route::where('user_id',$request->user_id)->get();
         $newRouteId=$userRoutes[count($userRoutes)-1]["id"];
         //Save data into table route_items
-        self::SavePlannedRouteItems($request,$newRouteId);
+        self::savePlannedRouteItems($request,$newRouteId);
         //Response
         return response()->json(["Message"=>"Ok","RouteId"=>$newRouteId],200);
     }
 
-    //Start route
-    //This function is called when user presses "Start" on "Route Overview with Map" screen
+    //Function startRoute(Request $request) is used when user starts shown route by 
+    //pressing "Start" on "Route Overview with Map" screen.
+    //  @request - Request that was received from user.
     public function startRoute(Request $request){
-        return self::ChangeRouteStatus($request,'2','Route started.');
+        return self::changeRouteStatus($request,'2','Route started.');
     }
 
-    //Finish route
-    //This function is called when user reaches his last landmark on map
+    //Function finishRoute(Request $request) is used when user reaches land mark on the map
+    //  @request - Request that was received from user.
     public function finishRoute(Request $request){
-        return self::ChangeRouteStatus($request,'3','Route finished successfully. Congratulations.');
+        return self::changeRouteStatus($request,'3','Route finished successfully. Congratulations.');
     }
 
-    //Suggested routes
-    //This function generates a suggested route based on random location.
+    //Function getSuggestedRoutes(Request $request, $place) 
+    //generates a suggested route based on random location.
     //User must send param place in his request and the algorithm will find
     //longitude and latitude of given place.
-    //After that, algorithm will search for all attractions with rating 3+ in range of 10 kilometers.
+    //After that, algorithm will search for all attractions with 
+    //google rating 3+ in range of 10 kilometers and create a route.
+    //  @request - Request that was received from user.
+    //  @place - Place for suggesting routes
     public function getSuggestedRoutes(Request $request,$place){
         //JWT validation
         if(!JWTValidation($request))
@@ -81,7 +85,7 @@ class RouteController extends Controller
         $longitude=json_encode($googleApiResponse->results[0]->geometry->location->lng);
 
         //Get nearby attractions
-        $tourist_attraction_json=self::GetNearby($latitude,$longitude,"tourist_attraction");
+        $tourist_attraction_json=self::getNearby($latitude,$longitude,"tourist_attraction");
         $amusement_park_json=self::getNearby($latitude,$longitude,"amusement_park");
         $museum_json=self::getNearby($latitude,$longitude,"museum");
         $library_json=self::getNearby($latitude,$longitude,"library");
@@ -95,6 +99,7 @@ class RouteController extends Controller
         $totalNumber=count($mergedArray);
         if($totalNumber<6)
             return response()->json("We're sorry, but this place does not have enough attractions to generate a route.");
+        
         //If number of attractions is greater than 25, then make it 25
         //because Google Directions API allows maximum 25 waypoints
         if($totalNumber>25)
@@ -156,23 +161,35 @@ class RouteController extends Controller
                                 200);
     }
 
-    //This function is used to 
-    //Get my planned route
+    //Function getPlannedRoutes(Request $request,$id) is used to get planned routes
+    //for user that requested his planned routes.
+    //  @request - Request that was received from user.
+    //  @id - user id
     public function getPlannedRoutes(Request $request,$id){
         return self::getSpecificGroupRoutes($request,'1',$id);
     }
 
-    //Get started routes
+    //Function getStartedRoutes(Request $request,$id) is used to get planned routes
+    //for user that requested his planned routes.
+    //  @request - Request that was received from user.
+    //  @id - user id
     public function getStartedRoutes(Request $request,$id){
         return self::getSpecificGroupRoutes($request,'2',$id);
     }
 
-    //Get my finished route
+    //Function getFinishedRoutes(Request $request,$id) is used to get planned routes
+    //for user that requested his planned routes.
+    //  @request - Request that was received from user.
+    //  @id - user id
     public function getFinishedRoutes(Request $request,$id){
         return self::getSpecificGroupRoutes($request,'3',$id);
     }
 
-    //This function is used to get certain status_id routes
+    //Function getSpecificGroupRoutes($request,$status_id,$user_id) is used to
+    //get to retrieve group routes based on route status_id.
+    //  @request - Request that was received from user.
+    //  @status_id - Route status id (1 - Planned, 2 - Started, 3 - Finished, 4 - Suggested)
+    //  @user_id - user_id which requested specific group routes
     public function getSpecificGroupRoutes($request,$status_id,$user_id){
         //JWT validation
         if(!JWTValidation($request)){
@@ -180,9 +197,8 @@ class RouteController extends Controller
         }else{
             //Check if user input is correct
             $user=UserModel::where('id',$user_id)->get();
-            if(count($user)==0){
+            if(count($user)==0)
                 return response()->json(["Message"=>"There is no user with given ID."],400);
-            }
 
             //Authenticate user request
             $email=$user[0]->email;
@@ -210,65 +226,25 @@ class RouteController extends Controller
                 array_push($plannedRoutes,$newItem);
             }
 
+            //Return response
             return response()->json($plannedRoutes,200);
         }
     }
 
-    //Get specific route by id -> MUST BE CHANGED
-    public function getSpecificRoute(Request $request,$id){
-        //JWT validation
-        if(!JWTValidation($request)){
-            return response()->json(["Error"=>"Unauthorized"],401);
-        }else{
-            //Get data from request
-            $route_id=$id;
-            
-            //Get route items
-            $route=Route::where('id',$route_id)->get();
-            $routeItems=RouteItem::where('route_id',$route_id)->get();  
-
-            //Put route items in routeItemsArray object
-            $routeItemsArray=array();
-            foreach($routeItems as $routeItem){
-                $routeItemObject=[
-                    "name"=>$routeItem->name,
-                    "place_id"=>$routeItem->place_reference,
-                    "latitude"=>$routeItem->latitude,
-                    "longitude"=>$routeItem->longitude,
-                    "duration"=>$routeItem->time,
-                    "distance"=>$routeItem->distance,
-                    "photo_reference"=>$routeItem->photo_reference
-                ];
-                array_push($routeItemsArray,$routeItemObject);
-            }
-
-
-            $specificRoute=[
-                "route_id"=>$route[0]->id,
-                "location"=>$route[0]->location,
-                "locations"=>$routeItemsArray,
-                "route"=>$route[0]->route,
-                "duration"=>$route[0]->total_time,
-                "distance"=>$route[0]->total_distance
-            ];
-
-            //Returning array of planned route objects
-            return $specificRoute;
-        }
-    }
-
-    //This function returns place description.
+    //Function getPlaceDescription(Request $request,$id) is used to get
+    //best review on location from Google API.
+    //  @request - Request that was received from user.
+    //  @id - Google Place API id
     public function getPlaceDescription(Request $request,$id){
-        if(!JWTValidation($request)){
+        if(!JWTValidation($request))
             return response()->json(["Error"=>"Unauthorized"],401);
-        }
 
         $apiResponse=file_get_contents('https://maps.googleapis.com/maps/api/place/details/json?place_id='.$id.'&key='.env("GOOGLE_API_KEY","somedefaultvalue"));
 
         //If there are no reviews about attraction
-        if(!isset(json_decode($apiResponse)->result->reviews)){
+        if(!isset(json_decode($apiResponse)->result->reviews))
             return response()->json(["description"=>"There is no comment about this attraction yet."],200);
-        }
+
         //Else take best rated comment
         $reviewsObject=json_decode($apiResponse)->result->reviews;
 
@@ -288,11 +264,18 @@ class RouteController extends Controller
     //-----------------------------------------------------------------------------------------------------------
     //---------------------------------  O T H E R   F U N C T I O N S ------------------------------------------
     //-----------------------------------------------------------------------------------------------------------
-    private function GetNearby($latitude,$longitude,$type){
+    //Function getNearby($latitude,$longitude,$type) is used to
+    //get specific nearby locations based on latitude and longitude coordinations.
+    //  @latitude - Latitude of current location. 
+    //  @longitude - Longitude of current location. 
+    //  @Type - Google API place type. 
+    private function getNearby($latitude,$longitude,$type){
+            //Form link and get data
             $link="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=".$latitude.",".$longitude."&radius=5000&type=".$type."&key=".env("GOOGLE_API_KEY","somedefaultvalue");
             $response=json_decode(file_get_contents($link));
             $response=$response->results;
 
+            //Create response array and fill it with data
             $responseArray=array();
             foreach($response as $r){
                     if(isset($r->photos[0]->photo_reference) && isset($r->rating)){
@@ -314,10 +297,17 @@ class RouteController extends Controller
                     array_push($responseArray,$ro);
                 }
             }
+
+            //Return response
             return $responseArray;
     }
 
-    private function ChangeRouteStatus($request,$status_id,$message){
+    //Function changeRouteStatus($request,$status_id,$message) is used to change
+    //route status id.
+    //  @request - Request that was received from user.
+    //  @status_id - New route status id.
+    //  @message - Message that will be returned to user.
+    private function changeRouteStatus($request,$status_id,$message){
         if(!JWTValidation($request))
             return response()->json(["Error"=>"Unauthorized"],401);
 
@@ -337,10 +327,15 @@ class RouteController extends Controller
 
         //Update
         Route::where('id',$route_id)->update(['status_id'=>$status_id]);
+
+        //Return response
         return response()->json(["Message"=>$message],200);
     }
 
-    private function SavePlannedRoute($request){
+    //Function savePlannedRoute($request) is used to save 
+    //planned route in database table "route".
+    //  @request - Request that was received from user.
+    private function savePlannedRoute($request){
         //Get info from request
         $location=$request->location;
         $user_id=$request->user_id;
@@ -360,7 +355,11 @@ class RouteController extends Controller
         $route->save();
     }
 
-    private function SavePlannedRouteItems($request,$routeId){
+    //Function savePlannedRoute($request) is used to save 
+    //planned route items in database table "route_items".
+    //  @request - Request that was received from user.
+    //  @routeId - foreign key to route to which route items belong.
+    private function savePlannedRouteItems($request,$routeId){
         //Get route items
         $locations=$request->locations;
         $order_counter=0;
