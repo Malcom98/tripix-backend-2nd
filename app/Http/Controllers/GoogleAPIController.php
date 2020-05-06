71b6b9e7727574d360eec5ef29a3c1f32cdacfd4
Malcom98 <35956934+Malcom98@users.noreply.github.com>
2020-05-06 21:39:21 +0200

    CODE REFACTORING - Improving getAttraction API functions and adding new functions for getAttractions.

--- app/Http/Controllers/GoogleAPIController.php
+++ app/Http/Controllers/GoogleAPIController.php
@@ -14,34 +14,40 @@ class GoogleAPIController extends Controller
     //------------------------------------- A P I    F U N C T I O N S ------------------------------------------
     //-----------------------------------------------------------------------------------------------------------
 
-    //-------------------------------------------------------------------
-    //------------------------ Google API Functions ---------------------
-    //-------------------------------------------------------------------
-
-    
-
+    //Function getNearbyRestaurants(Request $request) is used to get nearby restaurants based 
+    //on latitude and longitude sent in request.
+    //  @request - Request that was received from user.
     public function getNearbyRestaurants(Request $request){
         $types=array("restaurant");
-        $all_locations=self::getNearbyPlacesByTypes($request,$types);
+        $all_locations=self::getNearbyPlacesByType($request,$types);
         return response()->json($all_locations,200);
     }
 
+    //Function getNearbyRestaurants(Request $request) is used to get nearby cafes based 
+    //on latitude and longitude sent in request.
+    //  @request - Request that was received from user.
     public function getNearbyCafes(Request $request){
         $types=array("bar","cafe");
-        $nearby_locations=self::getNearbyPlacesByTypes($request,$types);
+        $nearby_locations=self::getNearbyPlacesByType($request,$types);
         return response()->json($nearby_locations,200);
     }
 
+    //Function getNearbyRestaurants(Request $request) is used to get nearby shops based 
+    //on latitude and longitude sent in request.
+    //  @request - Request that was received from user.
     public function getNearbyShops(Request $request){
         $types=array("shopping_mall","store","supermarket");
-        $nearby_locations=self::getNearbyPlacesByTypes($request,$types);
+        $nearby_locations=self::getNearbyPlacesByType($request,$types);
         return response()->json($nearby_locations,200);
     }
 
+    //Function getNearbyRestaurants(Request $request) is used to get nearby attractions based 
+    //on latitude and longitude sent in request.
+    //  @request - Request that was received from user.
     public function getNearbyAttractions(Request $request){
         $types=array("tourist_attraction","amusement_park","art_gallery","synagogue","city_hall",
                         "courthouse","embassy","museum","library","park","stadium");
-        $nearby_locations=self::getNearbyPlacesByTypes($request,$types);
+        $nearby_locations=self::getNearbyPlacesByType($request,$types);
         return response()->json($nearby_locations,200);
     }
 
@@ -93,90 +99,59 @@ class GoogleAPIController extends Controller
         return $citiesArray;
     }
 
-
-    //---------------------------- Get Attractions ---------------------------
-    //Function getAttraction(Request $request,$type) is used to get attractions 
-    //of specific type for location from request.
+    //Functions getAttractionParks(Request $request) is used to
+    //get park attractions in city requested in request params.
     //  @request - Request that was received from user.
-    //  @type - Google API place type.
-    public function getAttraction(Request $request,$type){
-        if(!JWTValidation($request))
-            return response()->json(["Error"=>"Unauthorized"],401);
-
-        $location=$request['location'];
-
-        if($location==null || $type==null)
-            return response()->json(["Error" => "Bad Request"],400);
-
-        $url="https://maps.googleapis.com/maps/api/place/textsearch/json?input=".rawurlencode($location)."&inputtype=textquery&type=".$type."&key=".env("GOOGLE_API_KEY","somedefaultvalue");
-        $response=json_decode(file_get_contents($url));
-        
-        //In needed response variable there is only info that is 
-        //needed for front end
-        $neededResponse=self::getAttractionInformationForFrontEnd($response);
-        return $neededResponse;
-    }
-
-    
     public function getAttractionParks(Request $request){
-        $park_json=self::getAttraction($request,"park");
-
-        self::DescendingSortByRating($park_json);
-        return response()->json($park_json,200);
+        $types=array("park");
+        $attractions=self::getAttractionsByType($request,$types);
+        return response()->json($attractions,200);
     }
 
+    //Functions getAttractionParks(Request $request) is used to
+    //get stadium attractions in city requested in request params.
+    //  @request - Request that was received from user.
     public function getAttractionStadiums(Request $request){
-        $stadium_json=self::getAttraction($request,"stadium");
-
-        self::DescendingSortByRating($stadium_json);
-        return response()->json($stadium_json,200);
+        $types=array("stadium");
+        $attractions=self::getAttractionsByType($request,$types);
+        return response()->json($attractions,200);
     }
 
+    //Functions getAttractionPets(Request $request) is used to
+    //get pet_store, aquarium and zoo attractions in city requested in request params.
+    //  @request - Request that was received from user.
     public function getAttractionPets(Request $request){
-        $zoo_json=self::getAttraction($request,"zoo");
-        $pet_store_json=self::getAttraction($request,"pet_store");
-        $aquarium_json=self::getAttraction($request,"aquarium");
-
-        $merged_json=array_merge($zoo_json,$pet_store_json,$aquarium_json);
-        self::DescendingSortByRating($merged_json);
-        $merged_json=RemoveDuplicates($merged_json);
-        return response()->json($merged_json,200);
+        $types=array("pet_store","aquarium","zoo");
+        $attractions=self::getAttractionsByType($request,$types);
+        return response()->json($attractions,200);
     }
     
+    //Functions getAttractionSchools(Request $request) is used to
+    //get school, university and library attractions in city requested in request params.
+    //  @request - Request that was received from user.
     public function getAttractionSchools(Request $request){
-        $school_json=self::getAttraction($request,"school");
-        $university_json=self::getAttraction($request,"university");
-        $library_json=self::getAttraction($request,"library");
-
-        $merged_json=array_merge($school_json,$university_json,$library_json);
-        self::DescendingSortByRating($merged_json);
-        $merged_json=RemoveDuplicates($merged_json);
-        return response()->json($merged_json,200);
+        $types=array("school","university","library");
+        $attractions=self::getAttractionsByType($request,$types);
+        return response()->json($attractions,200);
     }
 
+    //Functions getAttractionReligions(Request $request) is used to
+    //get church and synagogue attractions in city requested in request params.
+    //  @request - Request that was received from user.
     public function getAttractionReligions(Request $request){
-        $church_json=self::getAttraction($request,"church");
-        $synagogue_json=self::getAttraction($request,"synagogue");
-
-        $merged_json=array_merge($church_json,$synagogue_json);
-        self::DescendingSortByRating($merged_json);
-        $merged_json=RemoveDuplicates($merged_json);
-        return $merged_json;
+        $types=array("church","synagogue");
+        $attractions=self::getAttractionsByType($request,$types);
+        return response()->json($attractions,200);
     }
 
+    //Functions getAttractionLandmarks(Request $request) is used to
+    //get art gallery, city hall, courthouse, embassy and museum
+    // attractions in city requested in request params.
+    //  @request - Request that was received from user.
     public function getAttractionLandmarks(Request $request){
-        $tourist_attraction_json=self::getAttraction($request,"tourist_attraction");
-        $art_gallery_json=self::getAttraction($request,"art_gallery");
-        $city_hall_json=self::getAttraction($request,"city_hall");
-        $courthouse_json=self::getAttraction($request,"courthouse");
-        $embassy_json=self::getAttraction($request,"embassy");
-        $museum_json=self::getAttraction($request,"museum");
-
-        $merged_json=array_merge($tourist_attraction_json,$art_gallery_json,
-                $city_hall_json,$courthouse_json,$embassy_json,$museum_json);
-        self::DescendingSortByRating($merged_json);
-        $merged_json=RemoveDuplicates($merged_json);
-        return response()->json($merged_json,200);
+        $types=array("tourist_attraction","art_gallery","city_hall","courthouse","embassy","museum");
+        $attractions=self::getAttractionsByType($request,$types);
+        return response()->json($attractions,200);
     }
     
     //-----------------------------------------------------------------------------------------------------------
@@ -240,40 +215,6 @@ class GoogleAPIController extends Controller
         return $noviRating;
     }
 
-    //Function getAttractionInformationForFrontEnd($response) is function which
-    //gets only necessary information for frontend.
-    //  @response - Google API response from which we get only necessary data.
-    private function getAttractionInformationForFrontEnd($response){
-        $noviRating=self::bayesFormula($response);
-
-        //Vraćanje samo potrebnih informacija
-        $counter=0;
-        $neededInformation=array();
-        $places=((object)$response)->results;
-        foreach($places as $place){
-            //Obavezno koristiti isset umjesto !=null
-            if(isset($place->photos) && $place->user_ratings_total>=50){
-                $name=$place->name;
-                $photo_reference=$place->photos[0]->photo_reference;
-                $location=$place->geometry->location;
-                $place_id=$place->place_id;
-                $rating=$place->rating;
-                $object=[
-                    "name"=>$name,
-                    "photo_reference"=>$photo_reference,
-                    "location"=>$location,
-                    "place_id"=>$place_id,
-                   // "number_of_votes"=>$place->user_ratings_total,
-                    "rating"=>$noviRating[$counter] // Tu moram staviti nove ratingove, znaci moram ih prije izracunat
-                ];
-                $counter++;
-                array_push($neededInformation,$object);
-            }
-        }
-        
-        return $neededInformation;
-    }
-
     //Function DescendingSortByRating(&$array) sorts array in descending order
     //(higher rating at the top).
     //  @array - array of places for sorting.
@@ -283,22 +224,22 @@ class GoogleAPIController extends Controller
         });
     }
 
-    //Function getNearbyPlacesByTypes($request,&$types) is used to get nearby places for user
+    //Function getNearbyPlacesByType($request,&$types) is used to get nearby places for user
     //that is requesting specific Google API types (museums, amusement_parks, universities, etc).
     //  @request - Request that was received from user.
     //  @types - Array of google api types. (museums, amusement_parks, universities, etc)
-    private function getNearbyPlacesByTypes($request,&$types){
+    private function getNearbyPlacesByType($request,&$types){
         $all_locations=self::getNearby($request,$types[0]);
 
         for($i=1;$i<count($types);$i++){
             $new_locations=self::getNearby($request,$types[$i]);
-            array_merge($all_locations,$new_locations);
+            $all_locations=array_merge($all_locations,$new_locations);
         }
         $all_locations=RemoveDuplicates($all_locations);
+
         return $all_locations;
     }
 
-    //---------------------------- Get Nearby ---------------------------    
     //Function getNearby($latitude,$longitude,$type) is used to
     //get specific nearby locations based on latitude and longitude coordinations.
     //  @latitude - Latitude of current location. 
@@ -346,5 +287,77 @@ class GoogleAPIController extends Controller
 
         //Return response
         return $responseArray;
+    }
+
+    //Function getAttractionsByType($request,&$types) is used to get attraction places for user
+    //that is requesting specific Google API types (museums, amusement_parks, universities, etc).
+    //  @request - Request that was received from user.
+    //  @types - Array of google api types. (museums, amusement_parks, universities, etc)
+    private function getAttractionsByType($request,&$types){
+        $all_attractions=self::getAttraction($request,$types[0]);
+
+        for($i=1;$i<count($types);$i++){
+            $new_attractions=self::getAttraction($request,$types[$i]);
+            $all_attractions=array_merge($all_attractions,$new_attractions);
+        }
+
+        self::DescendingSortByRating($all_attractions);
+
+        $all_attractions=RemoveDuplicates($all_attractions);
+
+        return $all_attractions;
+    }
+
+    //Function getAttraction(Request $request,$type) is used to get attractions 
+    //of specific type for location from request.
+    //  @request - Request that was received from user.
+    //  @type - Google API place type.
+    public function getAttraction(Request $request,$type){
+        if(!JWTValidation($request))
+            return response()->json(["Error"=>"Unauthorized"],401);
+
+        $location=$request['location'];
+
+        if($location==null || $type==null)
+            return response()->json(["Error" => "Bad Request"],400);
+
+        $url="https://maps.googleapis.com/maps/api/place/textsearch/json?input=".rawurlencode($location)."&inputtype=textquery&type=".$type."&key=".env("GOOGLE_API_KEY","somedefaultvalue");
+        $response=json_decode(file_get_contents($url));
+        
+        //In needed response variable there is only info that is needed for front end
+        //
+        $neededResponse=self::getAttractionInformationForFrontEnd($response);
+        return $neededResponse;
+    }
+
+    //Function getAttractionInformationForFrontEnd($response) is function which
+    //gets only necessary information for frontend.
+    //  @response - Google API response from which we get only necessary data.
+    private function getAttractionInformationForFrontEnd($response){
+        $noviRating=self::bayesFormula($response);
+
+        $counter=0;
+        $neededInformation=array();
+        $places=((object)$response)->results;
+        foreach($places as $place){
+            if(isset($place->photos) && $place->user_ratings_total>=50){
+                $name=$place->name;
+                $photo_reference=$place->photos[0]->photo_reference;
+                $location=$place->geometry->location;
+                $place_id=$place->place_id;
+                $rating=$place->rating;
+                $object=[
+                    "name"=>$name,
+                    "photo_reference"=>$photo_reference,
+                    "location"=>$location,
+                    "place_id"=>$place_id,
+                    "rating"=>$noviRating[$counter] 
+                ];
+                $counter++;
+                array_push($neededInformation,$object);
+            }
+        }
+        
+        return $neededInformation;
     }
 }
