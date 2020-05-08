98b5264fb34821859e8ce1521753669593b107a4
Malcom98 <35956934+Malcom98@users.noreply.github.com>
2020-05-08 19:56:58 +0200

    MAIL - Uređivanje izgleda maila za aktivaciju accounta i reset passworda. (transparent slika)

--- resources/views/emails/activationEmail.blade.php
+++ resources/views/emails/activationEmail.blade.php
@@ -7,7 +7,7 @@
     <div style="border:2px solid black; border-radius:10px; background-color:#55C227; 
                 padding:0px 10px 10px 10px; text-align:center; color:white;">
         <h1 style="text-decoration:underline;">{{ $details['title'] }}</h1>
-        <img src="https://i.imgur.com/Ay4kuvR.jpg"/>
+        <img src="https://i.imgur.com/QXGYB2B.png"/>
         <p>{{ $details['body'] }}</p>
         <p style="font-size:16pt;fold-weight:bold; border:2px solid white;">{{ $details['code'] }}</p>
         <p>Thanks for using our application.  :-)</p>
